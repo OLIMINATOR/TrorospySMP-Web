a4c78a4ad7963c4f7ebbd4ee1a4e8add7c592e21
OLIMINATOR <oliminator@protonmail.com>
2021-11-08 21:28:44 +0100

    Very First Update For This Wholesome WEBSITE

---
+++ app/js/nwm naco tu si akoe ale ok – kópia.js
@@ -0,0 +1,15 @@
+console.log("Webiki by oliminator");
+console.log("Webiki by oliminator");
+console.log("Webiki by oliminator");
+console.log("Webiki by oliminator");
+console.log("Webiki by oliminator");
+console.log("Webiki by oliminator");
+console.log("");
+console.log("KED RIPOFUJES TAK PRAJEM VELA STASTIA");
+console.log("UVIDIS TEN NAJVIAC SHITTY KOD NA SVETE");
+console.log("GL");
+console.log("");
+console.log("uz som spomenul ze: Webiki by oliminator Webiki by oliminatorWebiki by oliminator?");
+function ipalert() {
+    alert("IP: trospysmp.cz");
+  }
