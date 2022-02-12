fadff6986472e5d38cfa11bffb76044748acaf3b
OLIMINATOR <oliminator@protonmail.com>
2022-02-12 21:36:14 +0100

    new pages

--- app/js/script.js
+++ app/js/script.js
@@ -1,4 +1,5 @@
 document.getElementById("server").innerHTML = "GTA";
+document.getElementById("whitelist").innerHTML = "ahoj";
 
 function ipalert() {
     alert("IP: trospysmp.cz");
--- dist/script.js
+++ dist/script.js
@@ -1,2 +1,2 @@
-"use strict";function ipalert(){alert("IP: trospysmp.cz")}document.getElementById("server").innerHTML="GTA";
+"use strict";function ipalert(){alert("IP: trospysmp.cz")}document.getElementById("server").innerHTML="GTA",document.getElementById("whitelist").innerHTML="ahoj";
 //# sourceMappingURL=script.js.map
