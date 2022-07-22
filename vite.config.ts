86d6d2231ae189a4daf38d9246534023ea1da16d
OLIMINATOR <oliminator@protonmail.com>
2022-07-22 21:52:24 +0200

    feat: 404 page

--- vite.config.ts
+++ vite.config.ts
@@ -54,6 +54,22 @@ export default defineConfig({
                         },
                     },
                 },
+                {
+                    filename: "404.html",
+                    template: "404.html",
+                    injectOptions: {
+                        data: {
+                            title: "TrospySMP - 404",
+                            description:
+                                "TrospySMP je minecraftový survival SMP server kde hrá mnoho YouTuberov ako Trospy, Hestreng, Zovicek, Medvid3k a další. Nájdeš tu všetky informácie a ako sa dostať na Trospysmp.",
+                            url: "https://trospysmp.cz/cleni",
+                            theme: "#4d4d4d",
+                            card_image:
+                                "https://trospysmp.cz/assets/pozadie.jpg",
+                            footer: "Trospysmp 1973 - 2020",
+                        },
+                    },
+                },
             ],
         }),
     ],
@@ -62,7 +78,8 @@ export default defineConfig({
             input: {
                 main: "./index.html",
                 whitelist: "./whitelist.html",
-                //cleni: "./cleni/index.html",
+                cleni: "./cleni/index.html",
+                404: "./404.html",
             },
         },
     },
